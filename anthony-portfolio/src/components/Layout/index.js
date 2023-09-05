797e54d60f904e2bf78865d4bb7ff485c0ffd926
DESKTOP-LGO4SDH\Anthony <s3780694@student.rmit.edu.au>
2023-09-06 01:20:51 +1000

    Added 'body' to layout

--- anthony-portfolio/src/components/Layout/index.js
+++ anthony-portfolio/src/components/Layout/index.js
@@ -1,10 +1,22 @@
 import './index.scss';
 import Navbar from '../Navbar';
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
     <div>
       <Navbar />
+      <div className='page'>
+        <span className='tags top-tags'>&lt;body&gt;</span>
+
+        <Outlet /> {/* Use the Outlet component from React Router for rendering nested routes */}
+
+        <span className='tags bottom-tags'>
+          &lt;/body&gt;
+          <br />
+          <span className='bottom-tag-html'></span>
+        </span>
+      </div>
     </div>
   );
 };
