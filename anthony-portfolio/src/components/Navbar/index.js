82e4a7678cfa68522634eed83b145a5d742b1753
DESKTOP-LGO4SDH\Anthony <s3780694@student.rmit.edu.au>
2023-09-06 00:00:58 +1000

    Navbar

--- anthony-portfolio/src/components/Navbar/index.js
+++ anthony-portfolio/src/components/Navbar/index.js
@@ -1,5 +1,26 @@
-import './index.scss'
+import './index.scss';
+import { Link, NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faInfoCircle, faEnvelope } from '@fortawesome/free-solid-svg-icons'; // Import the specific FontAwesome icons you need
+import Logo from '../../assets/images/Anthony-logos_transparent.png';
 
-const Navbar = () => (<> Hello from Navbar </>)
+const Navbar = () => (
+  <div className='nav-bar'>
+    <Link className='logo' to='/'>
+      <img src={Logo} alt="Logo" />
+    </Link>
+    <nav>
+      <NavLink exact={true} activeClassName="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink exact={true} activeClassName="active" to="/about">
+        <FontAwesomeIcon icon={faInfoCircle} color="#4d4d4e" />
+      </NavLink>
+      <NavLink exact={true} activeClassName="active" to="/contact">
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+  </div>
+);
 
-export default Navbar
+export default Navbar;
