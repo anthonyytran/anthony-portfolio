8df1c93549740961cfc6f3118005641c344ecd9d
DESKTOP-LGO4SDH\Anthony <s3780694@student.rmit.edu.au>
2023-09-06 23:37:28 +1000

    animated letters with hover effects

--- anthony-portfolio/src/components/Home/index.js
+++ anthony-portfolio/src/components/Home/index.js
@@ -2,37 +2,38 @@ import LogoA from '../../assets/images/A-logos_white.png';
 import { Link } from 'react-router-dom';
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Home = () => {
-    const [letterClass, setLetterClass] = useState('text-animate')
-    const nameArray = ['n', 't', 'h', 'o', 'n', 'y']
-    const jobArray = ['A', 's', 'p', 'i', 'r', 'i', 'n', 'g',' ', 'S','o','f','t','w','a','r','e', ' ', 'E','n','g','i','n','e','e','r']
+    const [letterClass, setLetterClass] = useState('text-animate');
+    const nameArray = ['n', 't', 'h', 'o', 'n', 'y'];
+    const jobArray = ['A', 's', 'p', 'i', 'r', 'i', 'n', 'g',' ', 'S','o','f','t','w','a','r','e', ' ', 'E','n','g','i','n','e','e','r'];
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLetterClass('text-animate-hover');
+        }, 4000);
+    }, []);
 
     return (
         <div className="container home-page">
             <div className="text-zone">
                 <h1>
-                <span className={letterClass}>H</span>
-                <span className={'${letterClass} _12'}>i,</span>
-                <br />
-                <span className={'${letterClass} _13'}>I</span>
-                <span className={'${letterClass} _14'}>'m</span>
-                <img src={LogoA} alt="developer" />
-                <AnimatedLetters letterClass={letterClass}
-                strArray={nameArray}
-                idx={15}/>
-                <br />
-                <AnimatedLetters letterClass={letterClass}
-                strArray={jobArray}
-                idx={22}/>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i,</span>
+                    <br />
+                    <span className={`${letterClass} _13`}>I</span>
+                    <span className={`${letterClass} _14`}>m</span>
+                    <img src={LogoA} alt="developer" />
+                    <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
+                    <br />
+                    <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={22} />
                 </h1>
                 <h2>Frontend Developer / Student / Gamer</h2>
-                <Link to ="/contact" className='flat-button'>CONTACT ME</Link>
+                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
         </div>
     );
 }
 
 export default Home;
-
