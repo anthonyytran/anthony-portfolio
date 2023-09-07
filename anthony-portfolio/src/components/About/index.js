9e7a4305db6445b364b802549df1e7f9e2fad4ef
DESKTOP-LGO4SDH\Anthony <s3780694@student.rmit.edu.au>
2023-09-07 16:46:21 +1000

    About me page

--- anthony-portfolio/src/components/About/index.js
+++ anthony-portfolio/src/components/About/index.js
@@ -1,5 +1,26 @@
 import './index.scss';
+import AnimatedLetters from '../AnimatedLetters'
 
-const About = () => 'Hello'
 
-export default About
+const About = () => {
+    return (
+        <div className='container about-page'>
+            <div className='text-zone'>
+                <h1>
+                    <AnimatedLetters
+                        strArray={['A','b','o','u','t',' ','M','e']}
+                        inx={15}
+                    />
+                </h1>
+                <p> Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
+                </p>
+                <p> Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
+                </p>
+                <p> Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default About;
