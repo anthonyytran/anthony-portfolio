915062cdd479d2031a42709fc3b472f6bcf9e017
DESKTOP-LGO4SDH\Anthony <s3780694@student.rmit.edu.au>
2023-09-10 03:43:45 +1000

    Added stars

--- anthony-portfolio/src/components/About/index.js
+++ anthony-portfolio/src/components/About/index.js
@@ -16,53 +16,61 @@ const About = () => {
 
     return (
         <>
-        <div className='container about-page'>
-            <div className='text-zone'>
-                <h1>
-                    <AnimatedLetters
-                        letterClass={letterClass}
-                        strArray={['A', 'b', 'o', 'u', 't', ' ', 'M', 'e']}
-                        inx={15}
-                        className={letterClass}
-                    />
-                </h1>
-                <p> Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
-                Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
-                Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
-                </p>
-                <p> Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
-                Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
-                Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
-                </p>
-                <p> Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum</p>
-            </div>
+            <div className='container about-page'>
+                <div className='text-zone'>
+                    <h1>
+                        <AnimatedLetters
+                            letterClass={letterClass}
+                            strArray={['A', 'b', 'o', 'u', 't', ' ', 'M', 'e']}
+                            inx={15}
+                            className={letterClass}
+                        />
+                    </h1>
+                    <p> Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
+                        Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
+                        Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
+                    </p>
+                    <p> Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
+                        Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
+                        Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum
+                    </p>
+                    <p> Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum</p>
+                </div>
 
-            <div className='stage-cube-cont'>
-                <div className='cubespinner'>
-                    <div className='face1'>
-                        <FontAwesomeIcon icon={faJava} style={{ color: '#DD0031' }} />
-                    </div>
-                    <div className='face2'>
-                        <FontAwesomeIcon icon={faHtml5} style={{ color: '#F06529' }} />
-                    </div>
-                    <div className='face3'>
-                        <FontAwesomeIcon icon={faCss3} style={{ color: '#28A4D9' }} />
-                    </div>
-                    <div className='face4'>
-                        <FontAwesomeIcon icon={faReact} style={{ color: '#5ED4F4' }} />
-                    </div>
-                    <div className='face5'>
-                        <FontAwesomeIcon icon={faJsSquare} style={{ color: '#EFD81D' }} />
-                    </div>
-                    <div className='face6'>
-                        <FontAwesomeIcon icon={faPython} style={{ color: '#28A4D9' }} />
+
+
+                <div className='stage-cube-cont'>
+                    
+                    <div id="stars"></div>
+                    <div id="stars2"></div>
+                    <div id="stars3"></div>
+
+                    <div className='cubespinner'>
+                        <div className='face1'>
+                            <FontAwesomeIcon icon={faJava} style={{ color: '#DD0031' }} />
+                        </div>
+                        <div className='face2'>
+                            <FontAwesomeIcon icon={faHtml5} style={{ color: '#F06529' }} />
+                        </div>
+                        <div className='face3'>
+                            <FontAwesomeIcon icon={faCss3} style={{ color: '#28A4D9' }} />
+                        </div>
+                        <div className='face4'>
+                            <FontAwesomeIcon icon={faReact} style={{ color: '#5ED4F4' }} />
+                        </div>
+                        <div className='face5'>
+                            <FontAwesomeIcon icon={faJsSquare} style={{ color: '#EFD81D' }} />
+                        </div>
+                        <div className='face6'>
+                            <FontAwesomeIcon icon={faPython} style={{ color: '#28A4D9' }} />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <Loader type="pacman" />
+            <Loader type="pacman" />
         </>
     );
 }
 
 export default About;
+
--- anthony-portfolio/src/components/Contact/index.js
+++ anthony-portfolio/src/components/Contact/index.js
@@ -1,17 +1,35 @@
 import './index.scss';
 import Loader from 'react-loaders';
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
-import { useState, useEffect } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate');
+    const refForm = useRef();
 
     useEffect(() => {
         setTimeout(() => {
             setLetterClass('text-animate-hover');
         }, 3000);
     }, []);
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm('contact_service', 'template_ooj0f7e', refForm.current, 'nyp4J0soNSf0P_J8EyWhS')
+            .then(
+                () => {
+                    alert('Message successfully sent!');
+                    window.location.reload(false);
+                },
+                () => {
+                    alert('Failed to send the message, please try again!');
+                }
+            );
+    };
+
     return (
         <>
             <div className='container contact-page'>
@@ -23,13 +41,10 @@ const Contact = () => {
                             indx={15}
                         />
                     </h1>
-                    <p>
-                        Feel free to use the below contact form to get into contact with me! 
-                    </p>
+                    <p>Feel free to use the below contact form to get in contact with me!</p>
 
                     <div className='contact-form'>
-                        <form>
-
+                        <form ref={refForm} onSubmit={sendEmail}>
                             <li className='half'>
                                 <input type="text" name="name" placeholder="Name" required />
                             </li>
@@ -37,17 +52,14 @@ const Contact = () => {
                                 <input type="email" name="email" placeholder="Email" required />
                             </li>
                             <li>
-                                <input placeholder="Subject" type="text" name="ubject" required />
+                                <input placeholder="Subject" type="text" name="subject" required />
                             </li>
                             <li>
-                                <textarea placeholder="Message" name="message" required>
-                                </textarea>
+                                <textarea placeholder="Message" name="message" required />
                             </li>
                             <li>
-                                <input type="submit" className="flat-button" value="SEND">
-                                </input>
+                                <input type="submit" className="flat-button" value="SEND" />
                             </li>
-
                         </form>
                     </div>
                 </div>
@@ -55,6 +67,6 @@ const Contact = () => {
             <Loader type="pacman" />
         </>
     );
-}
+};
 
 export default Contact;
