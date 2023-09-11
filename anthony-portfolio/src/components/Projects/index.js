9433d8393cceacfd7cadda731f89458af7cf8006
Anthony Tran <anthonytran@Anthonys-Air.localdomain>
2023-09-11 15:36:50 +1000

    Loaders to projects

--- anthony-portfolio/src/components/Projects/index.js
+++ anthony-portfolio/src/components/Projects/index.js
@@ -1,29 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import AnimatedLetters from '../AnimatedLetters';
+import React, { useState, useEffect } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
+import Loader from 'react-loaders'
 
 const Projects = () => {
-    const [letterClass, setLetterClass] = useState('text-animate');
+  const [letterClass, setLetterClass] = useState('text-animate')
 
-    useEffect(() => {
-        setTimeout(() => {
-            setLetterClass('text-animate-hover');
-        }, 3000);
-    }, []);
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
 
-    return (
-        <div className='container about-page'>
-            <div className='text-zone'>
-                <h1>
-                    <AnimatedLetters
-                        letterClass={letterClass}
-                        strArray={['P', 'r', 'o', 'j', 'e', 'c', 't', 's']}
-                        inx={15}
-                        className={letterClass}
-                    />
-                </h1>
-            </div>
-        </div>
-    );
+  return (
+    <div className="container projects-page">
+      <div className="text-zone">
+        <h1>
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={['P', 'r', 'o', 'j', 'e', 'c', 't', 's']}
+            inx={15}
+            className={letterClass}
+          />
+        </h1>
+      </div>
+      <Loader type="pacman" />
+    </div>
+  )
 }
 
-export default Projects;
+export default Projects
