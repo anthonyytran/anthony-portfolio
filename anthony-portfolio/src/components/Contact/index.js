fb46ee4e25d83883347e3116af8459ecf7ae6913
DESKTOP-LGO4SDH\Anthony <s3780694@student.rmit.edu.au>
2023-10-06 00:24:54 +1100

    Added map

--- anthony-portfolio/src/components/Contact/index.js
+++ anthony-portfolio/src/components/Contact/index.js
@@ -3,6 +3,7 @@ import Loader from 'react-loaders';
 import React, { useState, useEffect, useRef } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 import emailjs from '@emailjs/browser';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 
 
@@ -18,19 +19,19 @@ const Contact = () => {
 
     const sendEmail = (e) => {
         e.preventDefault()
-    
+
         emailjs
-          .sendForm('contact_service', 'contact_form', refForm.current, 'dLSkX7unlU8fSWDFm')
-          .then(
-            () => {
-              alert('Message successfully sent!')
-              window.location.reload(false)
-            },
-            () => {
-              alert('Failed to send the message, please try again')
-            }
-          )
-      }
+            .sendForm('contact_service', 'contact_form', refForm.current, 'dLSkX7unlU8fSWDFm')
+            .then(
+                () => {
+                    alert('Message successfully sent!')
+                    window.location.reload(false)
+                },
+                () => {
+                    alert('Failed to send the message, please try again')
+                }
+            )
+    }
 
     return (
         <>
@@ -64,6 +65,28 @@ const Contact = () => {
                             </li>
                         </form>
                     </div>
+                </div>
+                <div className="info-map">
+                    Anthony Tran,
+                    <br />
+                    Melbourne,
+                    <br />
+                    Australia <br />
+                    <br />
+                    <span>anthony.tran99@hotmail.com</span>
+                </div>
+                <div className="map-wrap">
+                    <MapContainer center={[	-37.840935, 144.946457]} zoom={10} scrollWheelZoom={false}>
+                        <TileLayer
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                        <Marker position={[	-37.840935, 144.946457]}>
+                            <Popup>
+                                Born and raised! <br /> 
+                            </Popup>
+                        </Marker>
+                    </MapContainer>
                 </div>
             </div>
             <Loader type="pacman" />
