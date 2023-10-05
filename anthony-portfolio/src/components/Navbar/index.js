af883ec2ef375ab7460579abae044cb812750e6d
DESKTOP-LGO4SDH\Anthony <s3780694@student.rmit.edu.au>
2023-10-05 18:19:41 +1100

    responsive navbar

--- anthony-portfolio/src/components/Navbar/index.js
+++ anthony-portfolio/src/components/Navbar/index.js
@@ -1,16 +1,20 @@
 import './index.scss';
 import { Link, NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faInfoCircle, faEnvelope, faSuitcase,} from '@fortawesome/free-solid-svg-icons';
+import { faHome, faInfoCircle, faEnvelope, faSuitcase, faBars, faClose} from '@fortawesome/free-solid-svg-icons';
 import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
 import AnthonyLogo from '../../assets/images/Anthony-logos_transparent.png';
+import { useState } from 'react';
 
-const Navbar = () => (
+const Navbar = () => {
+const [showNav, setShowNav] = useState(false);
+
+  return (
   <div className='nav-bar'>
     <Link className='logo' to='/'>
       <img src={AnthonyLogo} alt="Logo" />
     </Link>
-    <nav>
+    <nav className={showNav ? 'mobile-show': ''}>
       <NavLink exact={true} activeClassName="active" to="/">
         <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
       </NavLink>
@@ -23,6 +27,13 @@ const Navbar = () => (
       <NavLink exact={true} activeClassName="active" className="contact-link" to="/contact">
         <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
       </NavLink>
+      <FontAwesomeIcon
+        onClick={() => setShowNav(false)}
+        icon={faClose}
+        color="#ffd700"
+        size="3x"
+        className='close-icon'
+       />
     </nav>
     <ul>
       <li>
@@ -35,7 +46,15 @@ const Navbar = () => (
         </a>
       </li>
     </ul>
+    <FontAwesomeIcon
+      onclick={() => setShowNav(true)}
+      icon={faBars}
+      color="ffd700"
+      size="3x"
+      className='hamburger-icon'
+      />
   </div>
-);
+  )
+  };
 
 export default Navbar;
