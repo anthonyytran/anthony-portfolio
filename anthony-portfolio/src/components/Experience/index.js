0487b2e2f576b80401cd0a792a972783c529e701
DESKTOP-LGO4SDH\Anthony <s3780694@student.rmit.edu.au>
2024-02-14 01:18:20 +1100

    Experience page completed

--- anthony-portfolio/src/components/Experience/index.js
+++ anthony-portfolio/src/components/Experience/index.js
@@ -23,22 +23,48 @@ const Experience = () => {
             className={letterClass}
           />
         </h1>
+
         <div className="experience-content">
-          <h2 className="company-title">
-            Muse Scene Lab - Jul 2023 - Nov 2023
-          </h2>
-          <h3 className="position-title">Front-end Developer Intern</h3>
+          <h2 className="company-title">Muse Scene Lab</h2>
+          <h3 className="position-title">Front-End Developer Intern</h3>
+          <h3 className="position-date">Jul 2023 - Nov 2023</h3>
+          <div className="company-logo">
+            <img src="projects/companies/muse.png" alt="Company" />
+          </div>
           <div className="experience-description">
-            <p> test</p>
+            <p>
+              • Developed the music analysis platform with React for the
+              front-End and AWS DynamoDB for the back-End.
+              <br></br>• Implemented React-Vis library to provide graphical
+              representations of various music performance related metrics such
+              as tempo data and BPM.
+              <br></br>• Authored CI/CD pipelines to build, test and deploy
+              software using GitHub Actions.
+              <br></br>• Languages and technologies used: JavaScript, React,
+              REST API, JSON, AWS.
+            </p>
           </div>
         </div>
+
         <div className="experience-content">
-          <h2 className="company-title">
-            Inferno Software and Data - Nov 2022 - Feb 2023
-          </h2>
-          <h3 className="position-title">Software Engineer Intern</h3>
+          <h2 className="company-title">Inferno Software and Data</h2>
+          <h3 className="position-title">Software Developer Intern</h3>
+          <h3 className="position-date">Nov 2022 - Feb 2023</h3>
+          <div className="company-logo">
+            <img src="projects/companies/inferno.png" alt="Company" />
+          </div>
           <div className="experience-description">
-            <p> test</p>
+            <p>
+              • Rewrote legacy software using modern technologies and redesigned
+              APIs for modularity, maintainability and reusability.
+              <br></br>• Collaborated with cross-functional teams to design,
+              implement, and optimize robust software applications.
+              <br></br>• Proficiently managed software business operations by
+              excelling in client communication, sprint planning, PRD reviews,
+              and prioritization.
+              <br></br>• Languages and technologies used: JavaScript, React,
+              MySQL, REST API
+            </p>
           </div>
         </div>
       </div>
