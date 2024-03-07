29ac434a13047c8e44ec33bfd028b049fedd5900
DESKTOP-LGO4SDH\Anthony <s3780694@student.rmit.edu.au>
2024-03-07 14:40:11 +1100

    Added Google Analytics

--- anthony-portfolio/src/App.js
+++ anthony-portfolio/src/App.js
@@ -1,15 +1,35 @@
-import './App.scss';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; // Import BrowserRouter
-import Layout from './components/Layout';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
-import Projects from './components/Projects';
-import Experience from './components/Experience';
+import './App.scss'
+import { BrowserRouter, Routes, Route } from 'react-router-dom' // Import BrowserRouter
+import Layout from './components/Layout'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Projects from './components/Projects'
+import Experience from './components/Experience'
+import { Helmet } from 'react-helmet'
+
+const GoogleAnalytics = () => (
+  <Helmet>
+    {/* Global site tag (gtag.js) - Google Analytics */}
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=G-6S2RHVVWYY"
+    ></script>
+    <script>
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-6S2RHVVWYY');
+      `}
+    </script>
+  </Helmet>
+)
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}> 
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <GoogleAnalytics />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
@@ -20,7 +40,7 @@ function App() {
         </Route>
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
